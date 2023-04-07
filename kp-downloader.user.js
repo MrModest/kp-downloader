e12bd71c657cfebd7bed838116ae41f758fe39ba
MrModest <babaevkamil@gmail.com>
2023-04-07 18:49:14 +0200

    debug

--- kp-downloader.user.js
+++ kp-downloader.user.js
@@ -75,7 +75,7 @@
     wrapper.innerHTML = `
       <span class="dropdown">
         <button class="btn btn-secondary dropdown-toggle btn-outline-success m-b-sm" type="button" id="downloadButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Download (${e.fileName})
+          Download (${o.fileName})
         </button>
         <div class="dropdown-menu" aria-labelledby="downloadButton">
           ${
